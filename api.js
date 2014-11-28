f5f027e4773c3bdb235ba969b971393e33abfc8f
Sebastian Mai <bastinat0r@bastinat0r.de>
2014-11-28 22:30:20 +0100

    add script to regenerate client-count

---
+++ api.js
@@ -0,0 +1,56 @@
+#!/usr/bin/node
+var fs = require('fs');
+var util = require('util');
+var http = require('http');
+var https = require('https');
+
+var apiEntry = {}
+var nodes = {}
+
+
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
+function parseJsonFromFile(err, data) {
+	if(err) {
+		util.error(err);
+		process.exit(1);
+	}
+	try{
+		apiEntry = JSON.parse(data);
+	} catch (e) {
+		util.error("Could not parse JSON");
+		util.error(e);
+		process.exit(2);
+	}
+}
+
+function getNodesInformation(){
+	https.get("https://map.md.freifunk.net/nodes.json", function(res) {
+		data = "";
+		res.on('data', function(d) {
+			data = data + d;
+		});
+		res.on('end',function() {
+			nodes = JSON.parse(data);
+			updateNodeNumber(nodes);
+
+		});
+	});
+}
+
+function updateNodeNumber(nodes){
+	var routers = nodes.nodes.filter(function(node) {
+		return !node.flags.client & !node.flags.gateway & node.flags.online;
+	});
+	apiEntry.state.nodes = routers.length;
+	util.puts(util.inspect(apiEntry));
+}
+
+
+fs.readFile("ffmd.json", parseJsonFromFile);
+getNodesInformation();
+
+http.createServer(function(req, res) {
+	res.writeHead(200, {'content-type': 'application/javascript'});
+	res.end(JSON.stringify(apiEntry));
+}).listen(3000);
